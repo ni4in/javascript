05ade6a5a8fa02144aad9fd1655defdc7b36da0f
ni4in <to.nithinv@gmail.com>
2023-03-03 18:59:47 +0530

    added javascript files

--- data-structures/map-script.js
+++ data-structures/map-script.js
@@ -1,11 +1,33 @@
-const question = new Map([["question","what is the best pogramming language for datascience"],[1, "C"],[2, "Python"], [3, "java script"],["correct",3],[true,"Correct🏆"], [false,"Try Again 👍"]]);
+"use strict";
 
-const restuarant = new Set([1,2,3,4,1,2,3]);
+const question = new Map([
+  ["question", "what is the best pogramming language for datascience"],
+  [1, "C"],
+  [2, "Python"],
+  [3, "Javascript"],
+  ["correct", 2],
+  [true, "Correct🏆"],
+  [false, "Try Again 👍"],
+]);
 
-console.log(question);
-console.log(restuarant);
-prompt("what is your answer");
-for(const [key,value] of question){
-    console.log(key,value);
-};
+const restuarant = new Set([1, 2, 3, 4, 1, 2, 3]);
 
+// console.log(question);
+// console.log(restuarant);
+// const answer = Number(prompt("what is your answer"));
+// console.log(answer);
+// console.log(question.get(true,question.get("correct") === answer));
+
+for (const [key, value] of question) {
+  if (typeof key == "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+// const ans = Number(prompt("What is your answer ?"));
+const ans = 2;
+console.log(question.get(question.get("correct") === ans));
+
+console.log(question.entries());
+console.log(question.keys())
+console.log(question.values())
