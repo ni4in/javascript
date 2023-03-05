2dafe9e5a7d2f737921412f9231d205cc76b22cd
ni4in <to.nithinv@gmail.com>
2023-03-05 21:33:09 +0530

    added assignment4

--- data-structures/assignemt/script.js
+++ data-structures/assignemt/script.js
@@ -1,4 +1,19 @@
-"use strict"
+"use strict";
 
-console.log(document.querySelector(".yourName"))
+const myfunc = function () {
+  const mytext = document.querySelector("#text").value;
+  const mytext_split = mytext.split("\n");
+  let result_str = "";
+  for(const text of mytext_split) {
+    const temp_str = text.split("_");
+    const updated_str =
+      temp_str[0] +
+      temp_str[1][0].toUpperCase() +
+      temp_str[1].slice(1, temp_str[1].length).toLowerCase();
+      result_str = result_str + updated_str + "\n";
 
+  }
+  document.querySelector("#text").value = result_str;
+};
+
+document.querySelector("#button").addEventListener("click", myfunc);
